e8f50176e522d9a1b92cf456d3f139be584ab96a
saidureg <saidureg@gmail.com>
2023-09-21 18:59:42 +0600

    use Context API to avoid prop drilling

--- src/components/Family/Brother/Brother.jsx
+++ src/components/Family/Brother/Brother.jsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import { MoneyContext } from "../GrandFa/GrandFa";
+
 const Brother = () => {
+  const [money] = useContext(MoneyContext);
   return (
     <div>
       <h3>Brother</h3>
+      <p>GrandFa:{money} </p>
     </div>
   );
 };
--- src/components/Family/GrandFa/GrandFa.jsx
+++ src/components/Family/GrandFa/GrandFa.jsx
@@ -1,23 +1,28 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import Aunty from "../Aunty/Aunty";
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
 import "./GrandFa.css";
 
 export const AssetContext = createContext("gold");
+export const MoneyContext = createContext(1000);
 
 const GrandFa = () => {
+  const [money, setMoney] = useState(1000);
   const asset = "Diamond";
   return (
     <div className="grandFa">
       <h3>GrandFa</h3>
-      <AssetContext.Provider value="gold">
-        <section className="flex">
-          <Dad asset={asset}></Dad>
-          <Uncle asset={asset}></Uncle>
-          <Aunty></Aunty>
-        </section>
-      </AssetContext.Provider>
+      <p>Net asset:{money} </p>
+      <MoneyContext.Provider value={[money, setMoney]}>
+        <AssetContext.Provider value="gold">
+          <section className="flex">
+            <Dad asset={asset}></Dad>
+            <Uncle asset={asset}></Uncle>
+            <Aunty></Aunty>
+          </section>
+        </AssetContext.Provider>
+      </MoneyContext.Provider>
     </div>
   );
 };
